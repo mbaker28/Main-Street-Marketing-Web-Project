58d9f9e0bcdbef28fe0d45378f981471bacfb6d4
mkbaker <mattbakerk@gmail.com>
2016-12-06 09:30:40 -0600

    Created configuration file

---
+++ config.php
@@ -0,0 +1,24 @@
+<?php
+// db options
+define('DB_NAME', 'short_urls');
+define('DB_USER', 'matt');
+define('DB_PASSWORD', 'password');
+define('DB_HOST', 'localhost');
+define('DB_TABLE', 'shorturls');
+// connect to database
+mysql_connect(DB_HOST, DB_USER, DB_PASSWORD);
+mysql_select_db(DB_NAME);
+// base location of script (include trailing slash)
+define('BASE_HREF', 'http://' . $_SERVER['HTTP_HOST'] . '/');
+// change to limit short url creation to a single IP
+define('LIMIT_TO_IP', $_SERVER['REMOTE_ADDR']);
+// change to TRUE to start tracking referrals
+define('TRACK', FALSE);
+// check if URL exists first
+define('CHECK_URL', FALSE);
+// change the shortened URL allowed characters
+define('ALLOWED_CHARS', '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ');
+// do you want to cache?
+define('CACHE', TRUE);
+// if so, where will the cache files be stored? (include trailing slash)
+define('CACHE_DIR', dirname(__FILE__) . '/cache/');
