ea317ecc0dd2e7ff924e194bdf46ab984305560b
mkbaker <mattbakerk@gmail.com>
2016-12-08 00:11:19 -0600

    Editted redirect URL to fit to the Apache web server

--- shorten.php
+++ shorten.php
@@ -9,7 +9,7 @@ if(isset($_POST['url'])) {
     $url = $_POST['url'];
     
     if ($code = $s->makeCode($url)) {
-        $_SESSION['feedback'] = "Generated! Your short URL is: <a href=\"http://localhost:8000/{$code}\">http://localhost:8000/{$code}</a>";
+        $_SESSION['feedback'] = "Generated! Your short URL is: <a href=\"http://localhost:80/WebProject/{$code}\">http://localhost:80/WebProject/{$code}</a>";
     } else {
         $_SESSION['feedback'] = "There was a problem. Invalid URL, perhaps?";
     }
