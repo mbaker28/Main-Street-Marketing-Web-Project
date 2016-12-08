87a3994a29b714174327c0fb921cb8fee21cf6fc
mkbaker <mattbakerk@gmail.com>
2016-12-07 19:07:12 -0600

    Reconfigured php and added style sheet for future reference

---
+++ index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>URL Shortener</title>
+        <link rel="stylesheet" href="css/global.css">
+    </head>
+    <body>
+        <div class="container">
+            <h1 class="title">Shorten a URL.</h1>
+            
+            <form action="shorten.php" method="post">
+                <input type="url" name="url" placeholder="Enter a URL here." autocomplete="off">
+                <input type="submit" value="shorten">
+            </form>
+        </div>
+        <?php
+        // put your code here
+        ?>
+    </body>
+</html>
