24fa17e7242a0cab9abb4b201f131366ce8e837e
mkbaker <mattbakerk@gmail.com>
2016-12-07 19:11:23 -0600

    Changed font to Tahoma, sans-serif. Changed font-weight to normal

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
     <head>
         <meta charset="UTF-8">
         <title>URL Shortener</title>
-        <link rel="stylesheet" href="css/global.css">
+        <link rel="stylesheet" href="css/style.css">
     </head>
     <body>
         <div class="container">
